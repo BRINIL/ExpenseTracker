98fed15f60f82fc13a2106b66e4c5ec55d4d0b5c
annetjacob <123796920+annetjacob@users.noreply.github.com>
2023-05-05 19:19:27 +0530

    AddBudget
    Adding budget to the main page

--- src/ExpenseTrackerLogin.java
+++ src/ExpenseTrackerLogin.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -195,9 +196,124 @@ public class ExpenseTrackerLogin {
     public static void main(String[] args) {
         ExpenseTrackerLogin expenseTracker = new ExpenseTrackerLogin();
         expenseTracker.show();
+=======
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+public class ExpenseTrackerLogin extends JFrame implements AncestorListener {
+JLabel userLabel, passwordLabel;
+JTextField userTextField;
+JPasswordField passwordField;
+JButton loginButton, resetButton, signupButton;
+
+// Database connection parameters
+String url = "jdbc:mysql://localhost:3306/mydatabase";
+String username = "root";
+String password = "password";
+
+ExpenseTrackerLogin() {
+    userLabel = new JLabel("Username");
+    userLabel.setBounds(50, 50, 100, 30);
+
+    passwordLabel = new JLabel("Password");
+    passwordLabel.setBounds(50, 100, 100, 30);
+
+    userTextField = new JTextField();
+    userTextField.setBounds(150, 50, 150, 30);
+
+    passwordField = new JPasswordField();
+    passwordField.setBounds(150, 100, 150, 30);
+
+    loginButton = new JButton("Login");
+    loginButton.setBounds(50, 150, 100, 30);
+    loginButton.addActionListener(this);
+
+    resetButton = new JButton("Reset");
+    resetButton.setBounds(200, 150, 100, 30);
+    resetButton.addActionListener(this);
+
+    signupButton = new JButton("Sign up");
+    signupButton.setBounds(125, 200, 150, 30);
+    signupButton.addActionListener(this);
+
+    add(userLabel);
+    add(passwordLabel);
+    add(userTextField);
+    add(passwordField);
+    add(loginButton);
+    add(resetButton);
+    add(signupButton);
+
+    setTitle("Login Page");
+    setSize(400, 300);
+    setLayout(null);
+    setVisible(true);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+
+public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == loginButton) {
+        String user = userTextField.getText();
+        String password = new String(passwordField.getPassword());
+
+        if (user.equals("admin") && password.equals("admin123")) {
+            JOptionPane.showMessageDialog(this, "Login successful!");
+        } else {
+            JOptionPane.showMessageDialog(this, "Invalid username or password.");
+        }
+    } else if (e.getSource() == resetButton) {
+        userTextField.setText("");
+        passwordField.setText("");
+    } else if (e.getSource() == signupButton) {
+        String username = JOptionPane.showInputDialog(this, "Enter a new username:");
+        String password = JOptionPane.showInputDialog(this, "Enter a new password:");
+
+        // Add code here to store the new user details in the database
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+            PreparedStatement ps = con.prepareStatement("INSERT INTO users(username, password) VALUES (?, ?)");
+            ps.setString(1, username);
+            ps.setString(2, password);
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected > 0) {
+                JOptionPane.showMessageDialog(this, "New user created successfully!");
+            } else {
+                JOptionPane.showMessageDialog(this, "Failed to create new user.");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(this, "Error: " + ex.getMessage());
+        }
+>>>>>>> Stashed changes
     }
 }
 
+public static void main(String[] args) {
+    new ExpenseTrackerLogin();
+}
 
+<<<<<<< Updated upstream
 
     
+=======
+@Override
+public void ancestorAdded(AncestorEvent event) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'ancestorAdded'");
+}
+
+@Override
+public void ancestorRemoved(AncestorEvent event) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'ancestorRemoved'");
+}
+
+@Override
+public void ancestorMoved(AncestorEvent event) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'ancestorMoved'");
+}
+>>>>>>> Stashed changes
