bc413a966374c52c550c4e4c0e08fd184b9e0a47
Angitha <angitha.g08@gmail.com>
2023-05-05 17:33:54 +0530

    login
    code for login have been changed

--- src/ExpenseTrackerLogin.java
+++ src/ExpenseTrackerLogin.java
@@ -1,219 +1,172 @@
 import javax.swing.*;
-import javax.swing.table.TableModel;
-
 import java.awt.*;
-import java.awt.event.*;
-import java.sql.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class ExpenseTrackerLogin {
-    private JFrame loginFrame;
+    private JFrame frame;
     private JTextField usernameField;
     private JPasswordField passwordField;
+    private JTextField signupUsernameField;
+    private JPasswordField signupPasswordField;
+    private JTextField signupConfirmPasswordField;
+    private JButton loginButton;
+    private JButton signupButton;
+
+    private String currentUser;
 
     public ExpenseTrackerLogin() {
-        loginFrame = new JFrame("Expense Tracker - Login");
-        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Create the frame
+        frame = new JFrame("Expense Tracker");
+        frame.setSize(500, 300);
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.insets = new Insets(10, 10, 10, 10);
+        // Create the login form
+        JPanel loginPanel = new JPanel();
+        loginPanel.setLayout(new GridLayout(3, 2));
 
         JLabel usernameLabel = new JLabel("Username:");
-        panel.add(usernameLabel, constraints);
-
-        constraints.gridx = 1;
         usernameField = new JTextField();
-        panel.add(usernameField, constraints);
 
-        constraints.gridx = 0;
-        constraints.gridy = 1;
         JLabel passwordLabel = new JLabel("Password:");
-        panel.add(passwordLabel, constraints);
-
-        constraints.gridx = 1;
         passwordField = new JPasswordField();
-        panel.add(passwordField, constraints);
 
-        JButton loginButton = new JButton("Login");
+        loginPanel.add(usernameLabel);
+        loginPanel.add(usernameField);
+        loginPanel.add(passwordLabel);
+        loginPanel.add(passwordField);
+
+        // Create the login button
+        loginButton = new JButton("Login");
         loginButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                // Check if the credentials are valid
                 String username = usernameField.getText();
                 String password = new String(passwordField.getPassword());
-
-                if (username.isEmpty() || password.isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "Please enter a username and password.");
-                    return;
-                }
-
-                User user = authenticate(username, password);
-
-                if (user == null) {
-                    JOptionPane.showMessageDialog(null, "Incorrect username or password.");
+                if (validateUser(username, password)) {
+                    currentUser = username;
+                    openExpenseTracker();
                 } else {
-                    loginFrame.dispose();
-                    new ExpenseTrackerGUI(user).open();
+                    JOptionPane.showMessageDialog(frame, "Invalid username or password.");
                 }
             }
         });
 
-        constraints.gridx = 1;
-        constraints.gridy = 2;
-        constraints.insets = new Insets(20, 10, 10, 10);
-        panel.add(loginButton, constraints);
+        // Create the signup form
+        JPanel signupPanel = new JPanel();
+        signupPanel.setLayout(new GridLayout(4, 2));
 
-        loginFrame.add(panel);
-        loginFrame.pack();
-        loginFrame.setLocationRelativeTo(null);
-        loginFrame.setVisible(true);
+        JLabel signupUsernameLabel = new JLabel("Username:");
+        signupUsernameField = new JTextField();
+
+        JLabel signupPasswordLabel = new JLabel("Password:");
+        signupPasswordField = new JPasswordField();
+
+        JLabel signupConfirmPasswordLabel = new JLabel("Confirm Password:");
+        signupConfirmPasswordField = new JPasswordField();
+
+        signupPanel.add(signupUsernameLabel);
+        signupPanel.add(signupUsernameField);
+        signupPanel.add(signupPasswordLabel);
+        signupPanel.add(signupPasswordField);
+        signupPanel.add(signupConfirmPasswordLabel);
+        signupPanel.add(signupConfirmPasswordField);
+
+        // Create the signup button
+        signupButton = new JButton("Signup");
+        signupButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Validate the input fields and create a new user
+                String username = signupUsernameField.getText();
+                String password = new String(signupPasswordField.getPassword());
+                String confirmPassword = new String(((JPasswordField) signupConfirmPasswordField).getPassword());
+                if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                    JOptionPane.showMessageDialog(frame, "All fields are required.");
+                } else if (password.equals(confirmPassword)) {
+                    if (createUser(username, password)) {
+                        JOptionPane.showMessageDialog(frame, "User created successfully.");
+                    } else {
+                        JOptionPane.showMessageDialog(frame, "Username already taken.");
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Passwords do not match.");
+                }
+            }
+        });
+
+        // Create a tabbed pane to switch between login and signup
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.add("Login", loginPanel);
+        tabbedPane.add("Signup", signupPanel);
+
+        // Add the components to the frame
+        frame.add(tabbedPane, BorderLayout.CENTER);
+        frame.add(loginButton, BorderLayout.SOUTH);
     }
 
-    private User authenticate(String username, String password) {
-        String url = "jdbc:mysql://localhost:3306/expensetracker";
-        String user = "root";
-        String pass = "12345";
-
+    private boolean validateUser(String username, String password) {
+        // Open the user credentials file and check if the username and password match
         try {
-            Connection con = DriverManager.getConnection(url, user, pass);
-            String query = "SELECT * FROM user WHERE userID =? AND password =?";
-            PreparedStatement stmt = con.prepareStatement(query);
-            stmt.setString(1, username);
-            stmt.setString(2, password);
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                return new User();
+            File file = new File("users.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+                if (parts[0].equals(username) && parts[1].equals(password)) {
+                    return true;
+                }
             }
-
-            con.close();
-        } catch (SQLException e) {
+            scanner.close();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-        return null;
+        return false;
     }
-
+    
+    private boolean createUser(String username, String password) {
+        // Open the user credentials file and check if the username is taken
+        try {
+            File file = new File("users.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+                if (parts[0].equals(username)) {
+                    scanner.close();
+                    return false;
+                }
+            }
+            scanner.close();
+    
+            // Add the new user to the file
+            FileWriter writer = new FileWriter(file, true);
+            writer.write(username + "," + password + "\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+    
+    private void openExpenseTracker() {
+        // Implement the expense tracker GUI here
+        JOptionPane.showMessageDialog(frame, "Logged in as " + currentUser);
+    }
+    
+    public void show() {
+        frame.setVisible(true);
+    }
+    
     public static void main(String[] args) {
-        new ExpenseTrackerLogin();
+        ExpenseTrackerLogin expenseTracker = new ExpenseTrackerLogin();
+        expenseTracker.show();
     }
 }
 
-class ExpenseTrackerGUI {
-    private JFrame mainFrame;
-    private User user;
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    private JTable table;
-    private JTextField expenseField;
-    public JTextField getExpenseField() {
-        return expenseField;
-    }
-
-    public void setExpenseField(JTextField expenseField) {
-        this.expenseField = expenseField;
-    }
-
-    private JTextField amountField;
-
-    public JTextField getAmountField() {
-        return amountField;
-    }
-
-    public void setAmountField(JTextField amountField) {
-        this.amountField = amountField;
-    }
-
-    /**
-     * @param user
-     */
-    public ExpenseTrackerGUI(User user) {
-        this.user = user;
-        mainFrame = new JFrame("Expense Tracker - " + user.getUsername());
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel panel = new JPanel(new BorderLayout());
-
-        // Create table model and populate with user's expenses
-        ExpenseTableModel tableModel = new ExpenseTableModel();
-        tableModel.populate(user.getId());
-
-        table = new JTable((TableModel) tableModel);
-        JScrollPane scrollPane = new JScrollPane(table);
-        panel.add(scrollPane, BorderLayout.CENTER);
-
-        // Create button panel for adding expenses
-JPanel buttonPanel = new JPanel();
-buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
-
-JLabel balanceLabel = new JLabel("Balance: " + tableModel.getBalance());
-buttonPanel.add(balanceLabel);
-
-buttonPanel.add(Box.createHorizontalGlue());
-
-JButton addButton = new JButton("Add Expense");
-addButton.addActionListener(new ActionListener() {
-    public void actionPerformed(ActionEvent e) {
-        String description = JOptionPane.showInputDialog("Enter description:");
-        if (description == null || description.isEmpty()) {
-            return;
-        }
-
-        String amountStr = JOptionPane.showInputDialog("Enter amount:");
-        if (amountStr == null || amountStr.isEmpty()) {
-            return;
-        }
-
-        double amount;
-        try {
-            amount = Double.parseDouble(amountStr);
-        } catch (NumberFormatException ex) {
-            JOptionPane.showMessageDialog(null, "Invalid amount.");
-            return;
-        }
-
-        Expense expense = new Expense(description, amount, user.getId());
-        tableModel.addExpense(expense);
-        balanceLabel.setText("Balance: " + tableModel.getBalance());
-
-        // Save expense to database
-        String url = "jdbc:mysql://localhost:3306/expensetracker";
-        String user = "root";
-        String pass = "12345";
-
-        try {
-            Connection con = DriverManager.getConnection(url, user, pass);
-            String query = "INSERT INTO expenses (description, amount, user_id) VALUES (?, ?, ?)";
-            PreparedStatement stmt = con.prepareStatement(query);
-            stmt.setString(1, expense.getDescription());
-            stmt.setDouble(2, expense.getAmount());
-            stmt.setInt(3, expense.getUserId());
-            stmt.executeUpdate();
-
-            con.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
-});
-buttonPanel.add(addButton);
-
-panel.add(buttonPanel, BorderLayout.PAGE_END);
-
-mainFrame.add(panel);
-mainFrame.pack();
-mainFrame.setLocationRelativeTo(null);
-mainFrame.setVisible(true);
-    }
-
-    public void open() {
-    }
-}
+    
